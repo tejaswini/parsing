2ed8cb6582b26f40b714891137bccc012d28bbbd
Tejaswini <tejaswini@Asterix.(none)>
2013-10-20 17:16:38 -0400

    Changed the labels of edges to have headword, modified word and other details to build weights

--- parsing_hypergraph.py
+++ parsing_hypergraph.py
@@ -1,13 +1,31 @@
 import pprint
 import pydecode.hyper as ph
 import pydecode.display as display
+import cPickle as pickle
+
 
 c = {}
 comb = {}
 nodes = {}
 hyper1 = ph.Hypergraph()
+
+def init_all_dicts():
+    with open("data/initial_values","rb") as fp:
+        dep = pickle.load(fp)
+        cont = pickle.load(fp)
+        stop = pickle.load(fp)
+
+    return dep,cont,stop
+
+dep,cont,stop = init_all_dicts()
+
+pprint.pprint(dep)
+pprint.pprint(cont)
+pprint.pprint(stop)
+
 def eisners_algo(sentence):
-    n = len(sentence.split(" "))
+    words = sentence.split(" ")
+    n = len(words)
     with hyper1.builder() as b:
         for s in xrange(n):
             c[s,s,'right',1] = c[s,s,'left',1] = 0.5
@@ -22,40 +40,55 @@ def eisners_algo(sentence):
                 print "s is" + str(s) + " t is " + str(t)
                 # First: create incomplete items
                 c[s,t,'left',0] =  max([c[s,r, 'right', 1] + c[r+1,t,'left',1] for r in range(s,t)])
-                edges = [([nodes[s,r,'right',1],nodes[r+1,t,'left',1]],convert_to_string([s,r,'right',1])) for r in range(s,t)]
+                edges = [([nodes[s,r,'right',1],nodes[r+1,t,'left',1]],(words[t],words[s],'left',is_adj(t,s),1)) for r in range(s,t)]
                 nodes[s,t,'left',0] = b.add_node(edges, label=convert_to_string([s,t,'left',0]))
                 c[s,t,'right',0] = max([c[s,r,'right',1] + c[r + 1,t,'left',1] for r in range(s,t)])
-                edges = [([nodes[s,r,'right',1],nodes[r+1,t,'left',1]],convert_to_string([s,r,'left',1])) for r in range(s,t)]
-                nodes[s,t,'right',0] = b.add_node([([nodes[s,r,'right',1],nodes[r+1,t,'left',1]],convert_to_string([s,r,'right',1])) for r in range(s,t)], label=convert_to_string([s,t,'right',0]))
+                edges = [([nodes[s,r,'right',1],nodes[r+1,t,'left',1]],(words[s],words[t],'right',is_adj(s,t),1)) for r in range(s,t)]
+                nodes[s,t,'right',0] = b.add_node(edges, label=convert_to_string([s,t,'right',0]))
 
-           # Second: create complete items
+                # Second: create complete items
                 c[s,t,'left',1] = max([c[s,r,'left',1] + c[r,t,'left',0] for r in range(s,t)])
-                edges = [([nodes[s,r,'left',1],nodes[r,t,'left',0]],convert_to_string([s,r,'left',1])) for r in range(s,t)]
+                edges = [([nodes[s,r,'left',1],nodes[r,t,'left',0]],(words[t],words[s],'left',is_adj(t,s),1)) for r in range(s,t)]
                 nodes[s,t,'left',1] = b.add_node(edges,label=convert_to_string([s,t,'left',1]))
                 c[s,t,'right',1] = max([c[s,r,'right',0] + c[r,t,'right',1] for r in range(s+1,t+1)])
-                edges = [([nodes[s,r,'right',0],nodes[r,t,'right',1]],convert_to_string([s,r,'right',0])) for r in range(s+1,t+1)]
+                edges = [([nodes[s,r,'right',0],nodes[r,t,'right',1]],(words[s],words[t],'right',is_adj(s,t),1)) for r in range(s+1,t+1)]
                 nodes[s,t,'right',1] = b.add_node(edges,label=convert_to_string([s,t,'right',1]))
+
+                c[s,t,'left',2] = c[s,t,'left',1]
+                edges = [([nodes[s,r,'left',1]],(words[t],'','left',is_adj(t,s),1)) for r in range(s,t)]
+                nodes[s,t,'left',1] = b.add_node(edges,label=convert_to_string([s,t,'left',1]))
+                c[s,t,'right',1] = max([c[s,r,'right',0] + c[r,t,'right',1] for r in range(s+1,t+1)])
+                edges = [([nodes[s,r,'right',0],nodes[r,t,'right',1]],(words[s],words[t],'right',is_adj(s,t),1)) for r in range(s+1,t+1)]
+                nodes[s,t,'right',1] = b.add_node(edges,label=convert_to_string([s,t,'right',1]))
+
             
 
 def convert_to_string(array):
    return  ",".join(map(str,array))
 
 def is_adj(pos1,pos2):
-   0 if pos2-pos1==1 else  1
+  return "adj" if pos2-pos1==1 else "non-adj"
 
 def build_weights(label):
     head_word,modifier,direct,is_adj,is_cont=label 
-    if(is_cont):
-      return dep[head_word,modifier,direct,is_adj] * cont[head_word,is_adj]
-    # When head word is not empty, it means constit2
-    elif(head_word!=''):
-        return stop[head_word,is_adj]
+    if(is_cont and modifier!=''):
+      pprint.pprint(label)
+      return dep[head_word,modifier,direct,is_adj] * cont[head_word,direct,is_adj]
+    # When head words is not empty, it means constit2
+    elif(is_cont==0):
+        return stop[head_word,direct,is_adj]
     # When the tuple does not have any values, it means trap to constit
     else:
       return 1 
         
-
-eisners_algo("The dog barked")
+#eisners_algo("The dog barked")
+eisners_algo("A B C")
 pprint.pprint(c)    
 pprint.pprint(hyper1.edges())
+weights = ph.Weights(hyper1,build_weights)
+
+for edge in hyper1.edges():
+    label = hyper1.label(edge)
+    print hyper1.label(edge), build_weights(label)
+
 display.to_ipython(hyper1)
