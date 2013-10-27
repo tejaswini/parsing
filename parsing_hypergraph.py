2e5e63e46292f5df006b7ef9752ad7826de1c66b
Tejaswini <tejaswini@Asterix.(none)>
2013-10-27 19:29:24 -0400

    Modified Sasha's example to use latest pydecode implementation

--- parsing_hypergraph.py
+++ parsing_hypergraph.py
@@ -1,13 +1,16 @@
-import pprint
 import pydecode.hyper as ph
 import pydecode.display as display
+from collections import namedtuple, defaultdict
+import random
+import pydecode.chart as chart
 import cPickle as pickle
+import pprint
 
-
-c = {}
-comb = {}
-nodes = {}
-hyper1 = ph.Hypergraph()
+random.seed(0)
+Tri = "tri"
+Trap = "trap"
+Right = "right"
+Left = "left"
 
 def init_all_dicts():
     with open("data/initial_values","rb") as fp:
@@ -19,87 +22,89 @@ def init_all_dicts():
 
 dep,cont,stop = init_all_dicts()
 
-pprint.pprint(dep)
-pprint.pprint(cont)
-pprint.pprint(stop)
+class NodeType(namedtuple("NodeType", ["type", "dir", "span"])):
+    def __str__(self):
+        return "%s %s %d-%d"%(self.type, self.dir, self.span[0], self.span[1])
 
-def eisners_algo(sentence):
-    words = sentence.split(" ")
-    n = len(words)
-    with hyper1.builder() as b:
-        for s in xrange(n):
-            c[s,s,'right',1] = c[s,s,'left',1] = 0.5
-            nodes[s,s,'right',1] = b.add_node(label = convert_to_string([s,s,'right',1]))
-            nodes[s,s,'left',1] = b.add_node(label = convert_to_string([s,s,'left',1]))
+class Arc(namedtuple("Arc", ["head_word", "modifier_word","dir","is_adj","is_cont"])):
+    def __str__(self):
+        return "%s %s %s %s %d"%(self.head_word, self.modifier_word, self.dir, self.is_adj, self.is_cont)
 
-        for k in range(1,n+1):
-            for s in xrange(n):
-                t=s+k
-                if t >= n:
-                    continue
-                print "s is" + str(s) + " t is " + str(t)
-                # First: create incomplete items
-                c[s,t,'left',0] =  sum([c[s,r, 'right', 1] + c[r+1,t,'left',1] for r in range(s,t)])
-                edges = [([nodes[s,r,'right',1],nodes[r+1,t,'left',1]],(words[t],words[s],'left',is_adj(t,s),1)) for r in range(s,t)]
-                nodes[s,t,'left',0] = b.add_node(edges, label=convert_to_string([s,t,'left',0]))
-                
-                c[s,t,'right',0] = sum([c[s,r,'right',1] + c[r + 1,t,'left',1] for r in range(s,t)])
-                edges = [([nodes[s,r,'right',1],nodes[r+1,t,'left',1]],(words[s],words[t],'right',is_adj(s,t),1)) for r in range(s,t)]
-                nodes[s,t,'right',0] = b.add_node(edges, label=convert_to_string([s,t,'right',0]))
+def first_order(sentence, c):
+    tokens = sentence.split()
+    n = len(tokens)
 
-                # Second: create complete items
-                c[s,t,'left',1] = sum([c[s,r,'left',1] + c[r,t,'left',0] for r in range(s,t)])
-                edges = [([nodes[s,r,'left',1],nodes[r,t,'left',0]],(words[t],words[s],'left',is_adj(t,s),1)) for r in range(s,t)]
-                nodes[s,t,'left',1] = b.add_node(edges,label=convert_to_string([s,t,'left',1]))
-                
-                c[s,t,'right',1] = sum([c[s,r,'right',0] + c[r,t,'right',1] for r in range(s+1,t+1)])
-                edges = [([nodes[s,r,'right',0],nodes[r,t,'right',1]],(words[s],words[t],'right',is_adj(s,t),1)) for r in range(s+1,t+1)]
-                nodes[s,t,'right',1] = b.add_node(edges,label=convert_to_string([s,t,'right',1]))
+    # Add terminal nodes.
+    [c.init(NodeType(sh, d, (s, s)))
+     for s in range(n)
+     for d in [Right, Left]
+     for sh in [Trap, Tri]]
 
-                c[s,t,'left',2] = c[s,t,'left',1]
-                edges = [([nodes[s,r,'left',1]],(words[t],'','left',is_adj(t,s),1)) for r in range(s,t)]
-                nodes[s,t,'left',2] = b.add_node(edges,label=convert_to_string([s,t,'left',2]))
-                
-                c[s,t,'right',2] = c[s,t,'right',1]
-                edges = [([nodes[s,r,'right',1]],(words[s],'','right',is_adj(s,t),1)) for r in range(s+1,t+1)]
-                nodes[s,t,'right',2] = b.add_node(edges,label=convert_to_string([s,t,'right',2]))
+    for k in range(1, n):
+        for s in range(n):
+            t = k + s
+            if t >= n: break
+            span = (s, t)
 
-            
+            # First create incomplete items.
+            c[NodeType(Trap, Left, span)] = \
+                c.sum([c[NodeType(Tri, Right, (s, r))] * c[NodeType(Tri, Left, (r+1, t))] * \
+                   c.sr(Arc(tokens[t], tokens[s], Left, is_adj(t,s),1)) \
+                       for r in range(s, t)])
 
-def convert_to_string(array):
-   return  ",".join(map(str,array))
+            c[NodeType(Trap, Right, span)] = \
+                c.sum([c[NodeType(Tri, Right, (s, r))] * c[NodeType(Tri, Left, (r+1, t))] \
+                   * c.sr(Arc(tokens[s], tokens[t], Right, is_adj(t,s),1)) \
+                         for r in range(s, t)])
 
-def is_adj(pos1,pos2):
-  return "adj" if pos2-pos1==1 else "non-adj"
+            # Second create complete items.
+            c[NodeType(Tri, Left, span)] = \
+                c.sum([c[NodeType(Tri, Left, (s, r))] * \
+                       c[NodeType(Trap, Left, (r, t))] * \
+                c.sr(Arc(tokens[s], tokens[t], Right, is_adj(t,s),1)) \
+                       for r in range(s, t)])
 
-def build_weights(label):
-    head_word,modifier,direct,is_adj,is_cont=label 
-    if(is_cont and modifier!=''):
-      pprint.pprint(label)
-      return dep[head_word,modifier,direct,is_adj] * cont[head_word,direct,is_adj]
+            c[NodeType(Tri, Right, span)] = \
+                c.sum([c[NodeType(Trap, Right, (s, r))] * \
+                     c[NodeType(Tri, Right, (r, t))] * \
+                c.sr(Arc(tokens[s], tokens[t], Right, is_adj(t,s),1)) \
+                      for r in range(s + 1, t + 1)])
+    return c
+
+def build_weights(arc):
+    print "arc is"
+    pprint.pprint(arc)
+    if(arc.is_cont and arc.modifier_word!=''):
+      return dep[arc.head_word,arc.modifier_word,arc.dir,arc.is_adj] * \
+          cont[arc.head_word,arc.dir,arc.is_adj]
     # When head words is not empty, it means constit2
-    elif(is_cont==0):
-        return stop[head_word,direct,is_adj]
+    elif(arc.is_cont==0):
+        return stop[arc.head_word,arc.dir,arc.is_adj]
     # When the tuple does not have any values, it means trap to constit
     else:
       return 1 
-        
-#eisners_algo("The dog barked")
-eisners_algo("A B C")
-pprint.pprint(c)    
-pprint.pprint(hyper1.edges())
-weights = ph.Weights(hyper1,build_weights)
-#path,chart = ph.best_path(hyper1, weights)
 
-max_marginals = ph.compute_max_marginals(hyper1,weights)
-pprint.pprint(max_marginals)
+def is_adj(pos1,pos2):
+  return "adj" if abs(pos2-pos1)==1 else "non-adj"
+                      
+sentence  = "A B C"
+c = chart.ChartBuilder(lambda a: a,
+                       chart.HypergraphSemiRing,
+                       build_hypergraph = True)
+the_chart = first_order(sentence, c)
+hypergraph = the_chart.finish()
+
+weights = ph.Weights(hypergraph).build(build_weights)
+max_marginals = ph.compute_max_marginals(hypergraph, weights)
+
+for edge in hypergraph.edges:
+    label = hypergraph.label(edge)
+    print hypergraph.label(edge), build_weights(label)
+    print max_marginals[edge]
+
+print "nodes"
+
+for node in hypergraph.nodes:
+        print max_marginals[node]
 
 
-#best = weights.dot(path)
-
-
-for edge in hyper1.edges():
-    label = hyper1.label(edge)
-    print hyper1.label(edge), build_weights(label)
-
-display.to_ipython(hyper1)
