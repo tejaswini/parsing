010b405a794d5707e13c4317761f021837c37a89
Tejaswini <tejaswini@Asterix.(none)>
2013-11-26 11:47:04 -0500

    cont probability is now not 0

--- parser.py
+++ parser.py
@@ -4,6 +4,9 @@ from collections import defaultdict
 import cPickle as pickle
 from pickle_handler import PickleHandler
 
+def default_value():
+        return 0.0000000001
+
 class Parser:
 
     def __init__(self, corpus_path, initial_values_path):
@@ -22,7 +25,7 @@ class Parser:
                                             self.cont,self.stop)
 
     def run_em(self):
-        for i in range(30):
+        for i in range(2):
             self.dep_counts = defaultdict(float)
             self.stop_counts = defaultdict(float)
             for sentence in self.sentences:
@@ -34,53 +37,59 @@ class Parser:
                                    ["*"] + sentence.split())
 
             self.update_parameters()
-            self.pickle_handler.write_to_pickle(self.dep,self.cont,self.stop)
-            
+            self.pickle_handler.write_to_pickle(self.dep,self.cont,self.stop)            
 
     def update_counts(self, marginals, nodes, words):
         for node in nodes:
-            node_type, direct, span = node.label.split()
+            node_type, direct, span = str(node.label).split()
             span = span.split("-")
             values  = self.get_head_word(direct,span,
                                                     words)
-            head_word,modifier = values[0],values[1]
+            head_word, modifier = values[0], values[1]
             adj = self.is_adj(int(span[1]), int(span[0]))
             if(node_type == "trap"):
                 self.dep_counts[head_word, modifier, direct, adj] \
-                    += marginals[node].value
+                    += marginals[node]
             if(node_type == "triStop"):
                 self.stop_counts[head_word, direct, adj] +=  \
-                    marginals[node].value
+                    marginals[node]
 
     def get_head_word(self, direct, span, words):
         return (words[int(span[1])], words[int(span[0])]) \
             if direct=="left" else \
             (words[int(span[0])], words[int(span[1])])
-                
+
     def update_parameters(self):
-        self.dep = defaultdict(float)
-        self.stop = defaultdict(float)
-        self.cont = defaultdict(float)
+        self.dep = defaultdict(default_value) #defaultdict(float)
+        self.stop = defaultdict(default_value) #defaultdict(float)
+        self.cont = defaultdict(default_value) #defaultdict(float)
         for key in self.dep_counts.keys():
             values_trap = self.get_values_with_filter(key, self.dep_counts)
             total_trap = sum(values_trap)
+
             if(total_trap == 0):
                 continue
 
             self.dep[key] = self.dep_counts[key] / total_trap
+            if(key == ('DT', 'NNS', 'left', 'non-adj')):
+                print "total_trap", total_trap
+                print "dep",self.dep[key],"dep_counts",self.dep_counts[key]
 
             assert self.dep[key] <= 1, "%s,%s"%(self.dep_counts[key],total_trap)
-            
 
             stop_key = (key[0], key[2], key[3])
             self.stop[stop_key] = \
                 self.stop_counts[stop_key] / (total_trap + self.stop_counts[stop_key])
 
+            if(stop_key == ('DT', 'left', 'non-adj')):
+                print "stop value", self.stop[stop_key]
+                print "stop count",self.stop_counts[stop_key],"denom", (total_trap + self.stop_counts[stop_key])
+
             assert self.stop[stop_key] <= 1, "%s,%s"%(self.stop_counts[stop_key],total_trap)
 
-
-                
-            self.cont[stop_key] = 1 - self.stop[stop_key]
+            self.cont[stop_key] = total_trap / \
+                (total_trap + self.stop_counts[stop_key])
+#1 - self.stop[stop_key]
 
         for key in self.dep_counts.keys():
             values_prob = self.get_values_with_filter(key, self.dep)
@@ -90,10 +99,6 @@ class Parser:
 
 
     def get_values_with_filter(self, key, hash_table):
-        # return map(self.dep_counts.get, \
-        #                filter(lambda k: k[0] == key[0] 
-        #                       and key[2] == k[2] and k[3] == key[3], 
-        #                       self.dep_counts.keys()))
        return [val for key2,val in hash_table.iteritems()
                if key[0] == key2[0] and key[2] == key2[2]
                   and key2[3] == key[3]]
@@ -109,11 +114,11 @@ class Parser:
         return "adj" if abs(pos2-pos1) == 1 else "non-adj"
 
 if __name__ == "__main__":
-    parser = Parser("data/corpus.txt", "data/initial_values")
+    parser = Parser("data/corpus.txt", "data/harmonic_values") #"data/initial_values")
     parser.run_em()
     print "stop probalities are"
     pprint.pprint(parser.stop)
+    print "stop probalities are"
+    pprint.pprint(parser.cont)
     print "dep probalities are"
     pprint.pprint(parser.dep)
-    
-    
