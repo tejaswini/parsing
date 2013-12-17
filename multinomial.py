de114f7d94f778cfb00fcfee0fa2fee4cdd3d6aa
Tejaswini <tejaswini@Asterix.(none)>
2013-12-17 18:19:30 -0500

    Moved updating counts and estimating parameters to multinomials

---
+++ multinomial.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+class Multinomial:
+    def __init__(self):
+        self.prob = defaultdict(float)
+        self.counts = defaultdict(float)
+        self.total = 0
+
+    def inc_counts(self, instance, counts):
+        self.counts[instance] += counts
+        self.total += counts
+
+    def estimate(self):
+        self.prob = defaultdict(float)
+        if(self.total == 0):
+           self.counts = defaultdict(float)
+           return
+        
+        for instance in self.counts.keys():
+            self.prob[instance] = self.counts[instance] / self.total
+
+        self.counts = defaultdict(float)
+        self.total = 0
+    
