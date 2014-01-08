e5dbaea9aab71b2bb60a4edfafddde8f1b2eb262
Tejaswini <tejaswini@Asterix.(none)>
2014-01-08 02:29:27 -0500

    Evaluator keeps track of incorrect sentences and deps

--- evaluator.py
+++ evaluator.py
@@ -1,6 +1,8 @@
 import pprint
 from pickle_handler import PickleHandler
 from parsing_hypergraph import ParsingAlgo
+from collections import defaultdict
+import cPickle as pickle
 
 class Evaluator:
 
@@ -13,27 +15,55 @@ class Evaluator:
         self.directed_depen = 0
         self.undirected_depen = 0
         self.total_deps = 0
+        self.incorrect_sent = []
+        self.incorrect_dep = defaultdict(int)
 
     def evaluate_sentences(self):
         for i,sentence in enumerate(self.sentences):
+            actual_dep = self.dep_index[i].strip()
+            if len(sentence.split()) != len(actual_dep.split()):
+                   continue
+
             self.total_deps += len(sentence.split(" "))
             parsing_algo = ParsingAlgo(sentence.strip(), \
                       self.dep_mult_holder, self.cont_stop_mult_holder)
             depen = parsing_algo.best_edges()
-            actual_dep = self.dep_index[i].strip()
             self.evaluate_accuracy(depen, actual_dep.split(" "), \
                                    sentence)
         print "correct depen is " + str(self.directed_depen)
         print "total depen is " + str(self.total_deps)
-        print "accuracy is " + \
+        print "directed accuracy is " + \
               str(float(self.directed_depen) / self.total_deps)
+        print "undirected accuracy is " + \
+              str(float(self.undirected_depen) / self.total_deps)
 
     def evaluate_accuracy(self, depen, actual_dep, sentence):
+        sent_acc = 0
+        incorrect_dep_key = ("")
+        words = sentence.split()
         for key, value in depen.iteritems():
             pos_tag, index = key[0], key[1]
             dep_tag, dep_index = value[0], value[1]
             if(int(actual_dep[index-1]) == dep_index):
+                sent_acc += 1
                 self.directed_depen += 1
+                self.undirected_depen += 1
+            else:
+              if(dep_index!=0 \
+                     and int(actual_dep[dep_index-1]) == index):
+                self.undirected_depen += 1
+                sent_acc += 1
+              else:
+                  word = words[index-1]
+                  incorrect_dep = ("ROOT" if dep_index == 0\
+                                       else words[dep_index-1])
+                  correct_dep = ("ROOT" if int(actual_dep[index-1])\
+                      == 0  else words[(int(actual_dep[index-1]) - 1)])
+                  incorrect_dep_key = (word,incorrect_dep, correct_dep)
+      
+        if(sent_acc <= 2 and len(actual_dep) >= 2):
+            self.incorrect_sent.append(sentence)
+            self.incorrect_dep[incorrect_dep_key] += 1
 
     def get_sentences(self, file_path):
         sentences = []
@@ -41,11 +71,28 @@ class Evaluator:
             sentences = fp.readlines()
         return sentences
 
+    def write_to_file(self, file_name, data):
+        with open(file_name, "wb") as fp:
+            fp.writelines(("%s\n" % line for line in data))
+
 if __name__ == "__main__":
-    pickle_handler = PickleHandler("data/final_train")
+    pickle_handler = PickleHandler("data/dummy_final")
     dep_mult_holder, cont_stop_mult_holder =\
           pickle_handler.init_all_dicts()
-    evaluator = Evaluator("data/sentences_train_100.txt",
-        "data/dep_index_train_100.txt",dep_mult_holder,
+    evaluator = Evaluator("data/sample_sent.txt",
+       "data/sample_dep_index.txt", dep_mult_holder,
                           cont_stop_mult_holder)
+    # evaluator = Evaluator("one_word_sent",
+    #    "one_word_dep", dep_mult_holder,
+    #                       cont_stop_mult_holder)
+
     evaluator.evaluate_sentences()
+    evaluator.write_to_file("incorrect_sent_rule",
+                           evaluator.incorrect_sent)
+#    pprint.pprint(evaluator.incorrect_dep)
+    with open("incorrect_dep_dict_new", "wb") as fp:
+        pickle.dump(evaluator.incorrect_dep, fp)
+
+    
+    # evaluator.write_to_file("incorrect_dep_rule",
+    #                         evaluator.incorrect_dep)
