1b93f14f4f031adcb4035c08c20ccd54228eb853
Tejaswini <tejaswini@Asterix.(none)>
2014-01-10 21:35:02 -0500

    RandomInitializer's generate_multinomials now generates new intializations each time

--- random_initializer.py
+++ random_initializer.py
@@ -8,36 +8,33 @@ class RandomInitializer:
     def __init__(self, dep_mult, stop_cont_mult):
         self.harmonic_dep_mult = dep_mult
         self.harmonic_stop_cont_mult = stop_cont_mult
-        self.dep_mult_holder = MultinomialHolder()
-        self.stop_cont_mult_holder = MultinomialHolder()
 
     def initialize_multinomials(self):
-        self.initialize_dep()
-        self.initialize_stop_mult_cont()
-        total =  sum(self.dep_mult_holder.mult_list['VBZ', 'left'].prob.values())
-        assert round(total,1) == 1.0, "sum is "+ str(total)
-        total =  sum(self.dep_mult_holder.mult_list['NN', 'left'].prob.values())
-        assert round(total,1) == 1.0, "sum is "+ str(total)
-
+        dep_mult_holder = self.initialize_dep()
+        stop_cont_mult_holder = self.initialize_stop_mult_cont()
+        return dep_mult_holder, stop_cont_mult_holder
         
     def initialize_dep(self):
+        dep_mult_holder = MultinomialHolder()
         for cond_key, mult in self.harmonic_dep_mult.iteritems():
             for prob_key in mult.prob:
-                self.dep_mult_holder.\
+                dep_mult_holder.\
                     inc_counts(prob_key, cond_key, random.random())
 
-        self.dep_mult_holder.estimate()
-        
+        dep_mult_holder.estimate()
+        return dep_mult_holder
 
     def initialize_stop_mult_cont(self):
+        stop_cont_mult_holder = MultinomialHolder()
         for cond_key, mult in self.harmonic_stop_cont_mult.iteritems():
             random_value = random.random()
-            self.stop_cont_mult_holder.\
+            stop_cont_mult_holder.\
                     inc_counts(0, cond_key,random_value)
-            self.stop_cont_mult_holder.\
+            stop_cont_mult_holder.\
                     inc_counts(1, cond_key,1 - random_value)
 
-        self.stop_cont_mult_holder.estimate()
+        stop_cont_mult_holder.estimate()
+        return stop_cont_mult_holder
 
 if __name__ == "__main__":
     pickle_handler = PickleHandler("data/dummy")
@@ -45,4 +42,5 @@ if __name__ == "__main__":
     random_init = RandomInitializer(dep_mult, stop_cont_mult)
     random_init.initialize_multinomials()
     pickle_handler = PickleHandler("data/random_init")
-    pickle_handler.write_to_pickle(random_init.dep_mult_holder.mult_list, random_init.stop_cont_mult_holder.mult_list)
+    pickle_handler.write_to_pickle(random_init.dep_mult_holder.\
+          mult_list, random_init.stop_cont_mult_holder.mult_list)
